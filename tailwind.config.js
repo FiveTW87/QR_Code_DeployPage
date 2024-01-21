c778685d7b307d5d1ec883b32505ea676c513f6d
FiveTW87 <148542002+FiveTW87@users.noreply.github.com>
2024-01-21 20:32:25 +0700

    QR_CODE

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        Hanken: ['Hanken Grotesk', 'sans-serif'],
+      },
+      screens: {
+        'ss': {'max': '375px'}
+      },
+    },
+  },
+  plugins: [],
+};
